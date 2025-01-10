a22b19516803a2f6bb340c01a747dd7f2d546e87
Tarcyo <55460663+Tarcyo@users.noreply.github.com>
2025-01-10 14:09:49 -0300

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/ifg_mobile_estudante/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ifg_mobile_estudante
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ android/app/src/main/kotlin/com/example/ifg_mobile_estudante1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ifg_mobile_estudante1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
